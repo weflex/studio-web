a3b397b2235636c9e202102b4ae5605642841d88
Scott Wang <scott.wang@theweflex.com>
2015-07-14 10:52:22 +0800

    Use karma-launcher-nonet and angular v1.3

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
     // Which plugins to enable
     plugins: [
-      'karma-phantomjs-launcher',
+      'karma-phantomjs-launcher-nonet',
       'karma-coverage',
       'karma-jasmine'
     ],
