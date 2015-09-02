98f6e34941538a3c2af8f24571a3fb97001c71f4
Daixi <xi.dai1990@gmail.com>
2015-09-02 14:29:50 +0800

    Add daypasses list view

--- app/scripts/controllers/class-list.js
+++ app/scripts/controllers/class-list.js
@@ -3,6 +3,8 @@
 
 angular.module('weflexAdmin')
   .controller('ClassListCtrl', ['$scope', 'Classes', function($scope, Classes){
+    $scope.productType = 'class';
+
     Classes.query().$promise.then(function(classes) {
       $scope.classes = classes;
     });
