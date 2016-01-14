d181a61cfcb57ba1fed3a37d1ff8ccb2cd7748a7
yorkie <yorkiefixer@gmail.com>
2016-01-14 21:02:12 +0800

    update deps

--- app/orders/list.jsx
+++ app/orders/list.jsx
@@ -1,55 +1,102 @@
-'use strict';
+"use strict";
+require('./list.css');
+
 const React = require('react');
-const SortedTable = require('../sorted/table');
 const { SearchInput } = require('../toolbar/components/search');
 const client = require('@weflex/gian').getClient('dev');
 
-class OrderList extends SortedTable {
+class OrderList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      data: [],
+      selected: null
+    };
   }
-  async getDataSource() {
-    return await client.order.list();
-  }
-  componentDidMount() {
-    const table = this.refs.table;
-    SearchInput.Listen('onChange', table.filter.bind(table));
+  async componentDidMount() {
+    this.setState({
+      data: await client.order.list({
+        include: ['classPackage', 'user']
+      })
+    });
   }
   render() {
     return (
-      <SortedTable ref="table"
-        getDataSource={this.getDataSource}
-        tableHeight={window.innerHeight - 50}
-        tableWidth={window.innerWidth - 100}
-        columns={[
-          {
-            title: '课程',
-            key: 'prod.title.en'
-          },
-          {
-            title: '开课时间',
-            key: 'prod.from',
-            type: 'date'
-          },
-          {
-            title: '预订时间',
-            key: 'created',
-            type: 'date'
-          },
-          {
-            title: '用户',
-            key: 'user.nickname'
-          },
-          {
-            title: 'Passcode',
-            key: 'passcode'
-          },
-          {
-            title: '订单状态',
-            key: 'status'
-          }
-        ]}>
-      </SortedTable>
+      <div className="orders">
+        {this.renderListTable(this.state.data)}
+        {this.renderListView(this.state.data[this.state.selected])}
+      </div>
+    )
+  }
+  renderListTable(listData) {
+    return (
+      <div className="list-table">
+        <div className="list-table-header">
+          <div className="list-table-column">订单时间</div>
+          <div className="list-table-column">卡种选择</div>
+          <div className="list-table-column">客户</div>
+          <div className="list-table-column">有效期</div>
+        </div>
+        <div className="list-table-rows">
+          {listData.map((item, index) => {
+            return (
+              <div className="list-table-row" key={index} onClick={() => {
+                this.setState({selected: index})
+              }}>
+                <div className="list-table-column">{item.created}</div>
+                <div className="list-table-column">{item.productType}</div>
+                <div className="list-table-column">{item.user}</div>
+                <div className="list-table-column">{item.expiredDate}</div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+  renderListView(viewData) {
+    viewData = viewData || {};
+    return (
+      <div className="list-view">
+        <div className="list-view-header">订单详情</div>
+        <div className="list-view-content">
+          <div className="list-view-fieldset">
+            <label>订单时间</label>
+            <span>{viewData.created}</span>
+          </div>
+          <div className="list-view-fieldset">
+            <label>卡种类型</label>
+            <span>{viewData.productType}</span>
+          </div>
+          <div className="list-view-fieldset">
+            <label>有效期</label>
+            <span>{viewData.expiredDate}</span>
+          </div>
+          <div className="list-view-fieldset">
+            <label>使用次数</label>
+            <span>1次</span>
+          </div>
+          <div className="list-view-fieldset">
+            <label>剩余次数</label>
+            <span>1次</span>
+          </div>
+          <div className="list-view-fieldset">
+            <label>客户</label>
+            <span>{viewData.user}</span>
+          </div>
+          <div className="list-view-fieldset">
+            <label>付款方式</label>
+            <span>支付宝</span>
+          </div>
+          <div className="list-view-fieldset">
+            <label>金额</label>
+            <span>300元人民币</span>
+          </div>
+        </div>
+        <div className="list-view-footer">
+
+        </div>
+      </div>
     );
   }
 }
