f0532a7d4039c14c220f9c3a3d30a05515c45d3c
yorkie <yorkiefixer@gmail.com>
2016-02-01 18:13:50 +0800

    remove legacy

--- _legacy/app/config/prod.js
+++
@@ -1,6 +0,0 @@
-var weflex = weflex || {};
-
-weflex.env = {
-  API_URL: 'http://api.theweflex.com',
-  PORTAL_URL: 'http://api.theweflex.com'
-}
--- _legacy/app/config/staging.js
+++
@@ -1,6 +0,0 @@
-var weflex = weflex || {};
-
-weflex.env = {
-  API_URL: 'https://weflex-api-dev.herokuapp.com',
-  PORTAL_URL: 'http://api.theweflex.com'
-}
