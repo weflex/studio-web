78b634ecf8203f2183f1cd49994644769b81a6a8
yorkie <yorkiefixer@gmail.com>
2016-02-03 18:40:52 +0800

    update template

--- app/views/calendar/components/template.jsx
+++ app/views/calendar/components/template.jsx
@@ -1,30 +1,73 @@
 "use strict";
 
 import React from 'react';
+import ClassCard from '../card';
 import './template.css';
 
 class Template extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     this.state = {
-      name: props.data.name,
-      //TODO: add `duration`
-      trainer: props.data.trainer
+      showCopyCard: false
     };
   }
-
-  render () {
+  onMouseDown(event) {
+    this.setState({
+      showCopyCard: true
+    });
+  }
+  onMouseUp(event) {
+    this.setState({
+      showCopyCard: false
+    });
+  }
+  onUpdateCard(data) {
+    this.setState({
+      showCopyCard: false
+    });
+    this.props.onRelease(data);
+  }
+  render() {
+    let data = {
+      id: null,
+      from: { hour: 0, minute: 0 },
+      to: { hour: 1, minute: 30 },
+      template: this.props.template,
+      trainer: this.props.template.trainer,
+      orders: []
+    };
+    let cardStyle;
+    if (this.state.showCopyCard) {
+      cardStyle = {
+        opacity: 1
+      };
+    } else {
+      cardStyle = {
+        opacity: 0,
+        top: 0,
+        left: 0,
+        marginTop: 0,
+        marginLeft: 0
+      };
+    }
     return (
-      <li key={this.props.key} className="resource-calendar-template">
-        <div className='resource-calendar-template-name'>{this.state.name}</div>
-        <div className='resource-calendar-template-duration'>{'时长一小时'}</div>
-        <div className='resource-calendar-template-trainer'>
-          {this.state.trainer.fullname.first + ' ' + this.state.trainer.fullname.last}
-        </div>
+      <li className="resource-calendar-template">
+        <div>{this.props.template.name}</div>
+        <div>{'时长1小时'}</div>
+        <div>{this.props.template.trainer.fullname.first}</div>
         <div className="resource-calendar-template-hint">
-          <div className="icon-font icon-copy"></div>
+          <div className="icon-font icon-attach"></div>
           <div className="hint-text">拖动模版创建课程</div>
         </div>
+        <ClassCard
+          className="class-copy-card"
+          style={cardStyle}
+          cardInfo={data}
+          calendar={this.props.calendar}
+          onMouseDown={this.onMouseDown.bind(this)}
+          onMouseUp={this.onMouseUp.bind(this)}
+          updateCard={this.onUpdateCard.bind(this)}
+        />
       </li>
     );
   }
