336d4bdcffc5377565b8ddf3d0fa6ee5863aa355
yorkie <yorkiefixer@gmail.com>
2016-03-14 01:23:47 +0800

    v1.0.0-beta.0

--- app/api.js
+++ app/api.js
@@ -1,3 +1,7 @@
 "use strict";
 
-exports.client = require('@weflex/gian').getClient(process.env.GIAN_GATEWAY);
+exports.client = require('@weflex/gian').getClient(process.env.GIAN_GATEWAY, {
+  onAuthFail: (err) => {
+    window.location.href = '/login?msg=' + (err && err.message);
+  }
+});
