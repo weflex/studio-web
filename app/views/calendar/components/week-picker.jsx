807a67e29d6d6d3394179252599e7cbcce79d6c9
Scott Wang <me@scottoas.is>
2016-07-11 00:31:15 +0800

    Reuse Week Picker

--- app/views/calendar/components/week-picker.jsx
+++ app/views/calendar/components/week-picker.jsx
@@ -1,50 +1,65 @@
 import React from 'react';
 import moment from 'moment';
-import {
-  getWeek
-} from '../util';
-import './week-picker.css';
 import { SwitchTab } from './switch-tab';
 import { CCViewMode } from '../calendar-controller';
+import './week-picker.css';
 
 class WeekPicker extends React.Component {
   constructor(props) {
     super(props);
     this.calendar = this.props.calendar;
     this.state = {
-      viewDate: this.calendar.viewDate
+      viewDate: this.calendar.viewDate,
+      viewMode: this.calendar.viewMode
     };
     this.calendar.setPicker(this);
   }
 
-  goPrevWeek() {
+  goPrev() {
     const viewDate = this.calendar.viewDate;
-    const newDate = viewDate.subtract(7, 'days');
+    const viewMode = this.calendar.viewMode;
+    const newDate = viewDate.subtract(1, viewMode);
     this.setState({ viewDate: newDate });
     this.calendar.setViewDate(newDate);
   }
 
-  goNextWeek() {
+  goNext() {
     const viewDate = this.calendar.viewDate;
-    const newDate = viewDate.add(7, 'days');
+    const viewMode = this.calendar.viewMode;
+    const newDate = viewDate.add(1, viewMode);
     this.setState({ viewDate: newDate });
     this.calendar.setViewDate(newDate);
+  }
+
+  getTitleString () {
+    const viewDate = moment(this.calendar.viewDate);
+    const viewMode = this.calendar.viewMode;
+    const t = 'MM[月]DD[日]';
+    if (CCViewMode.week === viewMode) {
+      return viewDate.startOf('week').format(t) + ' - ' + viewDate.endOf('week').format(t);
+    } else {
+      return viewDate.format(t);
+    }
+  }
+
+  onSwitch (e) {
+    this.calendar.setViewMode(e);
+    this.setState({ viewMode: e });
   }
 
   render() {
-    const weekDate = getWeek(this.state.viewDate, 'MM[月]DD[日]');
     return (
       <div className="week-picker">
         <span className="go-prev-btn icon-font icon-left-open"
-          onClick={this.goPrevWeek.bind(this)}>
+          onClick={this.goPrev.bind(this)}>
         </span>
-        <div className="week-date">{weekDate.begin} - {weekDate.end}</div>
+        <div className="week-date">{ this.getTitleString() }</div>
         <span className="go-next-btn icon-font icon-right-open"
-          onClick={this.goNextWeek.bind(this)}>
+          onClick={this.goNext.bind(this)}>
         </span>
         <SwitchTab options={['周视图', '日视图']}
                    events={[CCViewMode.week, CCViewMode.day]}
-                   onSwitch={(e) => this.calendar.setViewMode(e)}/>
+                   onSwitch={(e) => this.onSwitch(e)}/>
       </div>
     );
   }
