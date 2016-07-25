3fda2d3d7709522467169b08c59599f536256888
Scott Wang <scott.wang@theweflex.com>
2016-07-25 19:21:54 +0800

    Unify hostname for Web Server and API Server

--- app/api.js
+++ app/api.js
@@ -1,6 +1,19 @@
 "use strict";
 
-exports.client = require('@weflex/gian').getClient(process.env.GIAN_GATEWAY, {
+const presets = {
+  "prod": {
+    "api": "https://studio.theweflex.com",
+    "app": "https://studio.theweflex.com",
+    "ws":  "ws://studio.theweflex.com"
+  }
+};
+let currentGateway = presets.prod;
+
+if (process.env.GIAN_GATEWAY && process.env.GIAN_GATEWAY in presets) {
+  currentGateway = presets[process.env.GIAN_GATEWAY];
+}
+
+exports.client = require('@weflex/gian').getClient(currentGateway, {
   onAuthFail: (err) => {
     window.location.href = '/login?msg=' + (err && err.message);
   }
