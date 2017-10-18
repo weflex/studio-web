3fb16ebde3499be48f3dc69c933cf689f718c662
chentaoyu <chentaoyu@theweflex.com>
2017-10-18 16:02:19 +0800

    限制上传教练头像格式和大小

--- app/components/ui-avatar/index.js
+++ app/components/ui-avatar/index.js
@@ -19,28 +19,48 @@ class AvatarUploader extends React.Component {
     });
   }
   async onAvatarUploaded(result, file) {
-    let dataSource = this.props.dataSource
-    await client.user.update(dataSource.userId, {
-      avatarId: result.id
-    }, '');
-    location.href = '/trainer/'+dataSource.id;
+    const isImage = this.checkFile(file)
+    if (!isImage) {
+      let dataSource = this.props.dataSource
+      await client.user.update(dataSource.userId, {
+        avatarId: result.id
+      }, '');
+      location.href = '/trainer/' + dataSource.id;
+    } else {
+      alert(isImage)
+    }
+  }
+  checkFile(file, index) {
+    const imageType = new Set(['image/png', 'image/jpg', 'image/jpeg', 'image/gif']);
+    if (!imageType.has(file.type)) {
+      return `文件(${file.name})不是图片格式：.png, .jpg, .jpeg, .gif。`;
+    } else if (file.size > 2 * 1024 * 1024) {
+      return `文件(${file.name})大小必须小于2M。`;
+    }
+    return null;
   }
   onError(err) {
     console.error(err);
   }
+  checkFileSize(file) {
+    console.log(file.size)
+  }
   render() {
     return (
       // <UIFramework.Image size={70} src={this.props.src} style={{marginRight: '10px'}} />
-      <UIFramework.Upload 
-        token={this.state.uptoken} 
-        onSuccess={this.onAvatarUploaded.bind(this)}
-        onError={this.onError}>
-        <UIFramework.Cell>
-          <UIFramework.Button>上传头像</UIFramework.Button>
-          <UIFramework.Divider />
-        </UIFramework.Cell>
-      </UIFramework.Upload>
+      <
+      UIFramework.Upload token = { this.state.uptoken }
+      onSuccess = { this.onAvatarUploaded.bind(this) }
+      onError = { this.onError } >
+      <
+      UIFramework.Cell >
+      <
+      UIFramework.Button > 上传头像 < /UIFramework.Button> <
+      UIFramework.Divider / >
+      <
+      /UIFramework.Cell> <
+      /UIFramework.Upload>
     );
   }
 }
-module.exports =  AvatarUploader
+module.exports = AvatarUploader
